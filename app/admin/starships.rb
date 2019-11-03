f6f28cf49813ec258a11b930eb549dcca9b72476
Anton Grygoriev <agrygoriev@Academic.RRC.ca>
2019-11-03 15:13:16 -0600

    Ran migration. Generated admin resources for Planet, Customer, Type and Starship models

---
+++ app/admin/starships.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Starship do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :name, :description, :speed, :length, :price, :is_new, :is_refurbished, :type_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:name, :description, :speed, :length, :price, :is_new, :is_refurbished, :type_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
