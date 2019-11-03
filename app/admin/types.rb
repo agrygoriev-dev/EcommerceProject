5141cd4bad602cf6ab7c285ec906dc1ed054cff9
Anton Grygoriev <agrygoriev@Academic.RRC.ca>
2019-11-03 17:24:57 -0600

    Created admin resources for customers and types

--- app/admin/types.rb
+++ app/admin/types.rb
@@ -1,18 +1,5 @@
-ActiveAdmin.register Type do
+# frozen_string_literal: true
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :name, :description
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:name, :description]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+ActiveAdmin.register Type do
+  permit_params :name, :description
 end
