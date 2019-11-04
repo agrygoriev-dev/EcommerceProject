2314c2c06b08c3e32cceac7da50ada7916e76d74
Anton Grygoriev <agrygoriev@Academic.RRC.ca>
2019-11-03 22:50:16 -0600

    Updated starship model for proper deletion

--- app/models/starship.rb
+++ app/models/starship.rb
@@ -2,9 +2,9 @@
 
 class Starship < ApplicationRecord
   belongs_to :type
-  has_many :shipplanets
+  has_many :shipplanets, dependent: :delete_all
   has_many :planets, through: :shipplanets
-  has_many :orderproducts
+  has_many :orderproducts, dependent: :delete_all
 
   validates :name, :price, presence: true
   validates :price, :length, :speed, numericality: true
