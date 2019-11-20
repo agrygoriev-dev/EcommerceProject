cbf58327f4166942d08c4510c14848c5c61a213c
Anton Grygoriev <agrygoriev@Academic.RRC.ca>
2019-11-19 18:10:42 -0600

    Generated User devise model. Added name column to User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   get 'starships/starship_refurbished', to: 'starships#starship_refurbished', as: :starship_refurbished
 
   get 'starships/starship_new', to: 'starships#starship_new', as: :starship_new
